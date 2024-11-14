ee0277a2d0601ccc1545a08ba482f9ee39a4e045
ShadowfeindX <shadowfeind@programmer.net>
2024-11-14 17:34:15 -0500

    setting new base point

--- dataconnect-generated/js/default-connector/index.cjs.js
+++
@@ -1,9 +0,0 @@
-const { getDataConnect, validateArgs } = require('firebase/data-connect');
-
-const connectorConfig = {
-  connector: 'default',
-  service: 'bard',
-  location: 'us-central1'
-};
-exports.connectorConfig = connectorConfig;
-
