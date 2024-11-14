1cfbc2f212aee21279c68c395bad4381f0eb886b
ShadowfeindX <shadowfeind@programmer.net>
2024-11-14 10:00:49 +0000

    Adding firebase features

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,8 @@
+import { getDataConnect, validateArgs } from 'firebase/data-connect';
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'bard',
+  location: 'us-central1'
+};
+
