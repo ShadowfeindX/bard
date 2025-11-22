534aa819a1042120cbf1defa3142b2d9ab0134ab
ShadowfeindX <shadowfeind@programmer.net>
2025-11-22 07:06:37 -0500

    disable hashing

--- src/lib/server/firebase.ts
+++ src/lib/server/firebase.ts
@@ -2,14 +2,15 @@ import { dev } from "$app/environment";
 import { getAuth } from "firebase-admin/auth";
 import { FirebaseScrypt, type FirebaseScryptOptions } from 'firebase-scrypt';
 
-let hash_config = new Function("return " + process.env.SCRYPT_HASH_CONFIG + ";")();
-const scrypt = new FirebaseScrypt(hash_config);
+// let hash_config = new Function("return " + process.env.SCRYPT_HASH_CONFIG + ";")();
+// const scrypt = new FirebaseScrypt(hash_config);
 
 console.log("server started...")
 
 export const auth = getAuth();
 
 const fakeHash = (context: string, salt: string) => Promise.resolve(`fakeHash:salt=${salt}:password=${context}`);
-const firebaseHash = (context: string, salt: string) => scrypt.hash(context, salt);
+// const firebaseHash = (context: string, salt: string) => scrypt.hash(context, salt);
+const firebaseHash = fakeHash;
 
 export const hash = dev ? fakeHash : firebaseHash;
